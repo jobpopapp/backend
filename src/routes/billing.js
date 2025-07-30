8ea1e783cab2cd82e1e184d6491f1d84464ba8ff
ugfire <ugfirecompany@gmail.com>
2025-07-30 21:45:33 +0300

    APP WORKS WELL, ADDED BILLING ADDRESS CRUD 30-JULY-205

--- src/routes/billing.js
+++ src/routes/billing.js
@@ -5,6 +5,7 @@ const { authenticateToken } = require("../middleware/auth");
 
 router.get("/", billingController.getBillingAddress);
 router.post("/", billingController.upsertBillingAddress);
+router.put("/:id", billingController.upsertBillingAddress);
 router.delete("/", billingController.deleteBillingAddress);
 
 module.exports = router;
